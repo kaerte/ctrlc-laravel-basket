18216fe038e5d33cb0a8c77151a4f12921f17757
kaerte <lucas.wski@gmail.com>
2021-07-18 12:21:05 +0200

    Add test_variant_has_productable

--- tests/Feature/ProductableTest.php
+++ tests/Feature/ProductableTest.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Ctrlc\Cart\Tests\Feature;
 
-use Ctrlc\Cart\Models\ProductVariant;
+use Ctrlc\Cart\Facades\Cart;
+use Ctrlc\Cart\ProductVariant;
 use Ctrlc\Cart\Tests\TestCase;
 use Ctrlc\Cart\Tests\User;
 use Illuminate\Database\Eloquent\Model;
@@ -31,5 +32,13 @@ class ProductableTest extends TestCase
     public function test_product_has_variant(): void
     {
         self::assertCount(1, $this->productable->variants);
+    }
+
+    public function test_variant_has_productable(): void
+    {
+        $productVariant = $this->productable->variants->first();
+        Cart::add($productVariant, 1);
+
+        self::assertSame($productVariant->productable->toArray(), Cart::items()->first()->item->productable->toArray());
     }
 }
